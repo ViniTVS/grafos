ab064b8a3887f7a79100fbdab44d1cec7865b6d1
Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>
2022-08-04 23:30:07 -0300

    Grafo completo(?)

--- grafo.c
+++ grafo.c
@@ -89,21 +89,49 @@ int grau_minimo(grafo g)  {
 }
 
 // -----------------------------------------------------------------------------
-int grau_medio(grafo g) {
-  
-  return 0;
+float grau_medio(grafo g) {
+  if(!g)
+    return -1;
+
+  int n_vertice, soma_graus = 0;
+  // a soma dos graus de todos os vértices = 2|E(G)|
+  for (vertice v = agfstnode(g); v; v = agnxtnode(g, v)){
+    n_vertice++;
+    soma_graus += grau(v, g);
+  }
+
+  return ((float)soma_graus/ (float)n_vertice);
 }
 
 // -----------------------------------------------------------------------------
 int regular(grafo g) {
-  
-  return 0;
+  // regular = todos os vértices de mesmo grau
+  if (!g)
+    return -1;
+
+  vertice v = agfstnode(g);
+  if (!v) // grafo não tem vértice
+    return -1;
+
+  int grau_v = grau(v, g);
+  // percorre todos os demais vértices
+  for (; v; v = agnxtnode(g, v)){
+    if (grau_v != grau(v,g))
+      return 0; // não é regular
+  }
+
+  return 1; // é regular
 }
 
 // -----------------------------------------------------------------------------
 int completo(grafo g) {
-  
-  return 0;
+  if(!g)
+    return 0;
+  int n = n_vertices(g);
+  int m = n_arestas(g);
+  // como só temos grafos direcionados, |A(G)| = n(n-1)/2 em grafos completos
+  //! também podemos verificar o grau 1 a 1 para ver se o grafo não foi "mal montado" 
+  return (m == n*(n-1)/2);
 }
 
 // -----------------------------------------------------------------------------
--- teste.c
+++ teste.c
@@ -10,21 +10,27 @@ int main(void)
 
   // escreve_grafo(g);
   int n = n_vertices(g);
-  printf("Vértices de g: %d \n", n);
+  printf("Vértices de G: %d \n", n);
   n = n_arestas(g);
-  printf("Arestas de g: %d \n", n);
+  printf("Arestas de G: %d \n", n);
   destroi_grafo(g);
   vertice v;
   // para cada nodo, obter seu grau
   for (v = agfstnode(g); v; v = agnxtnode(g, v)){
     n = grau(v, g);
-    printf("Grau de %s: %d \n", agnameof(v), n);
+    printf("Grau do vértice %s: %d \n", agnameof(v), n);
   }
 
   n = grau_maximo(g);
-  printf("Maior grau de g: %d \n", n);
+  printf("Maior grau de G: %d \n", n);
   n = grau_minimo(g);
-  printf("Menor grau de g: %d \n", n);
+  printf("Menor grau de G: %d \n", n);
+  float m = grau_medio(g);
+  printf("Grau médio de G: %f \n", m);
+  n = regular(g);
+  printf("G é regular? %d \n", n);
+  n = completo(g);
+  printf("G é completo? %d \n", n);
 
   return 0;
 }
